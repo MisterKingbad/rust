91b4c1764a13f9e2f893d62caa19d26fdcdd9d2e
MisterKingbad <davymichael20@gmail.com>
2024-01-11 14:45:02 -0300

    feat: :sparkles: Colocando input pelo terminal

---
+++ input/src/main.rs
@@ -0,0 +1,16 @@
+use std::io::stdin;
+fn convert_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+fn main() {
+    let mut numero = String::new();
+    stdin().read_line(&mut numero).expect("Erro ao ler numero");
+    let mut numero2 = String::new();
+    stdin().read_line(&mut numero2).expect("Erro ao ler numero");
+    if convert_to_int(&numero) > convert_to_int(&numero2) {
+        println!("O numero {} é maior que {}", numero, numero2);
+    } else {
+        println!("O numero {} é menor ou igual que {}", numero, numero2);
+    }
+  }
