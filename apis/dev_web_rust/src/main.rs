449b2f67f1df3d218b4b863bbc136e11f28b3706
MisterKingbad <davymichael20@gmail.com>
2024-01-29 13:29:49 -0300

    feat: :sparkles: Apis com rus usando rocket

--- apis/dev_web_rust/src/main.rs
+++ apis/dev_web_rust/src/main.rs
@@ -8,6 +8,30 @@ fn index() -> &'static str {
   "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+  format!("Hello, {}", name)
+}
+
+#[get("/number/<number>")]
+fn number(number: i32) -> String {
+  format!("o numero é: {}", number)
+}
+
+#[get("/search?<query>&<max_results>&<page>")]
+fn search(query: String, max_results: i32, page:i32) -> String {
+  format!("Searching for '{}' (max: {}, page: {})", query, max_results, page)
+}
+
+#[get("/search_two?<query>&<typ>")]
+fn search_two(query: String, typ: Option<String>) -> String {
+  match typ {
+    Some(t) => format!("Searching for '{}' (type: {})", query, t),
+    None => format!("Searching for '{}' (no type specified)", query),
+  }
+  
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, hello, number, search, search_two]).launch();
   }
