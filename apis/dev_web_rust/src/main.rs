9158c3e7e11174f383a2df8db81df2a579f544b5
MisterKingbad <davymichael20@gmail.com>
2024-01-30 13:27:31 -0300

    feat: :sparkles: novas rotas

--- apis/dev_web_rust/src/main.rs
+++ apis/dev_web_rust/src/main.rs
@@ -1,9 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::http::hyper::header::q;
-
 #[macro_use]
 extern crate rocket;
+use rocket::response::content;
+use rocket::State;
+use std::collections::HashMap;
+
+
+struct Local {
+  temperatura: f64,
+  cidade: String
+}
+
+fn obter_temperatura(local: &Local) -> String {
+  local.temperatura.to_string()
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -63,6 +74,36 @@ fn search_users(query: String, page: Option<u32>) -> String {
 }
 
 
+#[catch(404)]
+fn not_found() -> &'static str {
+  "Página não encontrada :("
+}
+
+#[get("/temperatura/<cidade>")]
+fn temperatura(local_state: State<Local>, cidade: String) -> content::Html<String>{
+  let local = local_state.inner();
+  if local.cidade == cidade {
+    content::Html(format!("A temperatura da cidade {} é de {}°C", cidade, obter_temperatura(local)))
+  } else {
+    content::Html(format!("Não foi possivel obter a temperatura da cidade {}", cidade))
+  }
+}
+
+#[get("/temperatura2/<cidade>")]
+fn temperatura2(local_state: State<HashMap<String,Local>>, cidade: String) -> content::Html<String>{
+  let local = local_state.inner();
+  match local.get(&cidade) {
+    Some(local) => content::Html(format!("A temperatura da cidade {} é de {}°C", cidade, obter_temperatura(local))),
+    None => content::Html(format!("Não foi possivel obter a temperatura da cidade {}", cidade))
+  }
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, hello, number, search, search_two, get_user, create_user, delete_user, update_user, search_users]).launch();
+
+    let mut local_state = HashMap::<String, Local>::new();
+    local_state.insert("São Paulo".to_string(), Local{temperatura: 25.5, cidade: "São Paulo".to_string()});
+    local_state.insert("Rio de Janeiro".to_string(), Local{temperatura: 35.5, cidade: "Rio de Janeiro".to_string()});
+    local_state.insert("Belo Horizonte".to_string(), Local{temperatura: 30.5, cidade: "Belo Horizonte".to_string()});
+
+    rocket::ignite().mount("/", routes![index, hello, number, search, search_two, get_user, create_user, delete_user, update_user, search_users, temperatura, temperatura2]).register(catchers![not_found]).manage(local_state).launch();
   }
