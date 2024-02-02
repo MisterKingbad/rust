f6f0cca2443023ee3a57fde7491038e0920a25b1
MisterKingbad <davymichael20@gmail.com>
2024-02-02 09:00:52 -0300

    feat: :memo: atualizando

--- apis/dev_web_rust/src/main.rs
+++ apis/dev_web_rust/src/main.rs
@@ -1,11 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-use rocket::response::content;
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::json::Json;
+use rocket::http::{ContentType, Status};
+use rocket::response::{Content, content};
 use rocket::State;
 use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Serialize, Deserialize)]
+struct FormInput {
+  name: String,
+  email: String,
+}
 
 struct Local {
   temperatura: f64,
@@ -98,6 +108,86 @@ fn temperatura2(local_state: State<HashMap<String,Local>>, cidade: String) -> co
   }
 }
 
+
+#[get("/form")]
+fn form() -> Content<String> {
+  
+  let html = r#"
+      <!DOCTYPE html>
+      <html>
+          <head>
+              <title> Formulario </title>
+          </head>
+          <body>
+            <h1> Exemplo de Formulario</h1>
+            <form method="post" action="/form">
+              <label for="name">Nome:</label>
+              <input type="text" name="name">
+              <br>
+              <label for="email">E-mail:</label>
+              <input type="email" name="email">
+              <br>
+              <button type="submit">Enviar</button>
+            </form>
+
+          </body>
+      </html>
+  "#;
+  Content(ContentType::HTML, html.to_string())
+
+}
+
+#[post("/form", data="<form_input>")]
+fn subimit_form(form_input: Json<FormInput>) -> Result<Content<String>, Status> {
+  match validate_input(&form_input)  {
+   Ok(_) => {
+      let msg = format!("Name: {}\n Email: {}", form_input.name, form_input.email);
+      let html = format!(r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title> Formulario </title>
+            </head>
+            <body>
+              <h1> Sucesso! </h1>
+              <p>{}</p>
+
+            </body>
+        </html>
+      "#, msg);
+      Ok(Content(ContentType::HTML, html.to_string()))
+   },
+   Err(error) => {
+    let html = format!(r#"
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title> Formulario </title>
+        </head>
+        <body>
+          <h1> Erro! </h1>
+          <p>{}</p>
+          <p><a href="/form">Voltar</a></p>
+        </body>
+    </html>
+  "#, error);
+    Err(Status::UnprocessableEntity)
+   }    
+  }
+}
+
+fn validate_input(form_input: &FormInput) -> Result<(), String> {
+  if form_input.name.is_empty() {
+    return Err("Name is required".to_string());
+  }
+
+  if form_input.email.is_empty() {
+    return  Err("Email is required".to_string());
+  }
+
+  Ok(())
+}
+
 fn main() {
 
     let mut local_state = HashMap::<String, Local>::new();
@@ -105,5 +195,6 @@ fn main() {
     local_state.insert("Rio de Janeiro".to_string(), Local{temperatura: 35.5, cidade: "Rio de Janeiro".to_string()});
     local_state.insert("Belo Horizonte".to_string(), Local{temperatura: 30.5, cidade: "Belo Horizonte".to_string()});
 
-    rocket::ignite().mount("/", routes![index, hello, number, search, search_two, get_user, create_user, delete_user, update_user, search_users, temperatura, temperatura2]).register(catchers![not_found]).manage(local_state).launch();
+    rocket::ignite().mount("/", routes![index, hello, number, search, search_two, get_user, create_user, delete_user, update_user, search_users, temperatura, temperatura2, form, subimit_form]).register(catchers![not_found]).manage(local_state).launch();
   }
+
